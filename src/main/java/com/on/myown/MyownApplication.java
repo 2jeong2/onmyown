aebcf6596d173340149b2808a720773359f17258
DESKTOP-THLL0VQ\eje93 <jeongyi0131@naver.com>
2022-02-26 18:44:04 +0900

    [이정이]sts myown 환경세팅

---
+++ src/main/java/com/on/myown/MyownApplication.java
@@ -0,0 +1,15 @@
+package com.on.myown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class MyownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyownApplication.class, args);
+	}
+
+}
